bf84c56bc6136ebf14d2c37228fe0a80cb7b57fa
Jonathan de Jong <jonathan@tigerton.se>
2017-05-17 00:50:45 +0200

    initial commit. First working version

---
+++ spec/atom-wpcodex-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomWpcodexView from '../lib/atom-wpcodex-view';
+
+describe('AtomWpcodexView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
